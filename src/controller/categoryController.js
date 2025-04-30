de234d349da0c718463be510a7e4e04ec28aeb8d
Ajit <yajit1908@gmail.com>
2025-04-30 20:37:07 +0530

    Added the delete api for the Category

--- src/controller/categoryController.js
+++ src/controller/categoryController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from "http-status-codes";
-import {  createCategory } from "../service/categoryService.js";
+import {  createCategory, deletecategory } from "../service/categoryService.js";
 import { internalErrorResponse, successResponse } from "../utils/Common/CommonResponse.js";
+import { CategoryRepository } from "../repository/categoryRepository.js";
 
 
 export const createCategoryController = async(req,res)=>{
@@ -15,3 +16,24 @@ export const createCategoryController = async(req,res)=>{
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(internalErrorResponse(error));
     }
 }
+
+
+export const deleteCategoryController = async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      const deletedCategory = await deletecategory(id);
+  
+      if (!deletedCategory) {
+        return res.status(404).json({ message: "Category not found" });
+      }
+  
+      return res.status(200).json({
+        message: "Category deleted successfully",
+        category: deletedCategory,
+      });
+    } catch (error) {
+      console.error("Error in deleting category:", error.message);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  };
--- src/routes/v1/category.js
+++ src/routes/v1/category.js
@@ -1,10 +1,11 @@
-import { createCategoryController } from "../../controller/categoryController.js";
+import { createCategoryController, deleteCategoryController } from "../../controller/categoryController.js";
 import { isAuthenticated } from "../../middleware/isAuthmiddleware.js";
 import express from 'express'
 
 const router = express.Router();
 
-router.post('/',isAuthenticated,createCategoryController);
+router.post('/create',isAuthenticated,createCategoryController);
+router.delete('/delete/:id',isAuthenticated,deleteCategoryController);
 
 
 export default router;
