0dabebf4b7d2665f1e7f431bdc7918437c15ce2f
Ajit <yajit1908@gmail.com>
2025-05-04 19:19:49 +0530

    Added public get for the product and category or added rate limit

--- src/routes/v1/product.js
+++ src/routes/v1/product.js
@@ -2,14 +2,15 @@ import express from 'express'
 import { createproductcontroller, deleteProductController, getPoductAllContoller, getPoductByIdContoller, getPresignedUrl, updateProductController } from '../../controller/productController.js';
 import { isAuthenticated } from '../../middleware/isAuthmiddleware.js';
 import { authorizeRoles } from '../../middleware/rolemiddleware.js';
+import { getLimiter } from '../../utils/Common/apilimit.js';
 
 const router = express.Router();
 
 router.post('/createproduct/:categoryId',isAuthenticated,authorizeRoles('admin'),createproductcontroller);
 router.get('/getproduct/:productId',isAuthenticated,authorizeRoles('admin'),getPoductByIdContoller);
-router.put('/updateproduct/:id',isAuthenticated,authorizeRoles('admin'),updateProductController);
+router.put('/updateproduct/:id',isAuthenticated,authorizeRoles('admin,customer'),updateProductController);
 router.delete('/deleteproduct/:id',isAuthenticated,authorizeRoles('admin'),deleteProductController);
-router.get('/getall',isAuthenticated,authorizeRoles('customer'),getPoductAllContoller);
-router.get('/getpre',isAuthenticated,authorizeRoles('customer'),getPresignedUrl);
+router.get('/getall',getLimiter,getPoductAllContoller);
+router.get('/getpre',getLimiter,getPresignedUrl);
 
 export default router;
